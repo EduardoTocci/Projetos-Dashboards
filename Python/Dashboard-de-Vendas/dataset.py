024de22343af6e76f2e55e01c567c2ebebcd5145
Eduardo Tocci <eduardo.lago.tocci@gmail.com>
2025-06-17 22:04:22 -0300

    Correção de localização do arquivo

--- Python/Dashboard-de-Vendas/dataset.py
+++ Python/Dashboard-de-Vendas/dataset.py
@@ -1,14 +1,16 @@
 import json
 import pandas as pd
+import os
 
-file = open("./dados/vendas.json")
-data = json.load(file)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, "dados", "vendas.json")
 
-# chamando o pandas para tabular o json
+# Abrindo e carregando o JSON no mesmo bloco
+with open(file_path, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# Convertendo para DataFrame
 df = pd.DataFrame.from_dict(data)
 
- # formatando de string para data
+# Formatando a coluna de datas
 df['Data da Compra'] = pd.to_datetime(df['Data da Compra'], format='%d/%m/%Y')
-
-
-file.close()
